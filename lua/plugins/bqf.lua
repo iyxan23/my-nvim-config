7fd776d7a3a603f72278c2e5beab658066908108
iyxan23 <nurihsanalghifari@gmail.com>
2024-08-31 19:43:45 +0700

    chore: make the quickfix window look better by setting vim.o.qftf

---
+++ lua/plugins/bqf.lua
@@ -0,0 +1,11 @@
+return {
+  "kevinhwang91/nvim-bqf",
+  event = "VeryLazy",
+  config = {
+    filter = {
+      fzf = {
+        extra_opts = { "--bind", "ctrl-o:toggle-all", "--delimiter", "│" },
+      },
+    },
+  },
+}
